40da8c5ccfc9267487d03b3ae1277e5367fa51bd
Ganes556 <gustiganes@gmail.com>
2023-02-26 10:15:19 +0800

    [Tugas] fixing folder name & test case

--- 7_Time Complexity & Space Complexity/pratikum/1. explorasi/eksplorasi-2/main.go
+++ 7_Time Complexity & Space Complexity/pratikum/1. explorasi/eksplorasi-2/main.go
@@ -17,6 +17,10 @@ func pow(x, n int) int {
 }
 
 func main() {
-	fmt.Println(pow(2,8))
+	fmt.Println(pow(2,3))
+	fmt.Println(pow(5,3))
+	fmt.Println(pow(10,2))
+	fmt.Println(pow(2,5))
+	fmt.Println(pow(7,3))
 
 }
