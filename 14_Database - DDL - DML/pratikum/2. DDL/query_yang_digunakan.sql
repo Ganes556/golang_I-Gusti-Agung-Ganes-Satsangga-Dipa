ac4e2de52ce96f3e3bfe4e18d064d1fd34f5c015
Ganes556 <gustiganes@gmail.com>
2023-03-18 15:34:51 +0800

    [Tugas] Section 14 skema-database & ddl

--- 14_Database - DDL - DML/pratikum/2. DDL/query_yang_digunakan.sql
+++ 14_Database - DDL - DML/pratikum/2. DDL/query_yang_digunakan.sql
@@ -2,14 +2,16 @@
 -- CREATE DATABASE alta_online_shop
 CREATE DATABASE alta_online_shop;
 
+USE alta_online_shop;
+
 -- Implementasi schema Olshop yang telah dikerjakan
 
 -- 1-to-many: user dengan alamat.
 CREATE TABLE user_addresses (
   address_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   address VARCHAR(255),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE users (
@@ -17,32 +19,32 @@ CREATE TABLE users (
   user_address_id INT NOT NULL,
   name VARCHAR(255) NOT NULL,
   gender CHAR(1) NOT NULL,
-  date_of_birth DATETIME,
-  status ENUM('active', 'inactive', 'blocked' ,'deleted'),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
+  date_of_birth DATE,
+  status ENUM('active', 'inactive') DEFAULT 'active',
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
   FOREIGN KEY (user_address_id) REFERENCES user_addresses(address_id)
 );
 
 CREATE TABLE product_descriptions (
   product_description_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   description TEXT,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE product_types (
   product_type_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   name VARCHAR(50),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE product_operators (
   product_operator_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   name VARCHAR(50),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE products (
@@ -53,8 +55,8 @@ CREATE TABLE products (
   name VARCHAR(100) NOT NULL,
   price FLOAT NOT NULL,
   stock INT NOT NULL,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
   FOREIGN KEY (product_description_id) REFERENCES product_descriptions(product_description_id),
   FOREIGN KEY (product_type_id) REFERENCES product_types(product_type_id),
   FOREIGN KEY (product_operator_id) REFERENCES product_operators(product_operator_id)
@@ -64,17 +66,17 @@ CREATE TABLE products (
 CREATE TABLE payment_descriptions (
   payment_description_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   description TEXT,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE payment_methods (
   payment_method_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   payment_description_id INT NOT NULL,
   name VARCHAR(50),
-  status ENUM('active', 'inactive'),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
+  status ENUM('active', 'inactive') DEFAULT 'active',
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
   FOREIGN KEY (payment_description_id) REFERENCES payment_descriptions(payment_description_id)
 );
 
@@ -83,22 +85,22 @@ CREATE TABLE user_payment_method_details (
   user_payment_method_detail_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   user_id INT NOT NULL,
   payment_method_id INT NOT NULL,
-  card_number VARCHAR(50),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
+  payment_number VARCHAR(50),
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(user_id),
   FOREIGN KEY (payment_method_id) REFERENCES payment_methods(payment_method_id)
-)
+);
 
 CREATE TABLE transactions (
   transaction_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   payment_method_id INT NOT NULL,
   user_id INT NOT NULL,
   total_qty INT NOT NULL,
-  total_price FLOAT NOT NULL,
-  status ENUM('pending', 'paid', 'canceled', 'delivered'),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
+  total_price DOUBLE NOT NULL,
+  status ENUM('unpaid', 'paid') DEFAULT 'unpaid',
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(user_id),
   FOREIGN KEY (payment_method_id) REFERENCES payment_methods(payment_method_id)
 );
@@ -108,9 +110,9 @@ CREATE TABLE transaction_details (
   transaction_id INT NOT NULL,
   product_id INT NOT NULL,
   qty INT NOT NULL,
-  price FLOAT NOT NULL,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
+  price DOUBLE NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
   FOREIGN KEY (transaction_id) REFERENCES transactions(transaction_id),
   FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
@@ -120,8 +122,8 @@ CREATE TABLE transaction_details (
 CREATE TABLE kurir (
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   name VARCHAR(50),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
 );
 
 -- Tambahkan ongkos_dasar column di tabel kurir
