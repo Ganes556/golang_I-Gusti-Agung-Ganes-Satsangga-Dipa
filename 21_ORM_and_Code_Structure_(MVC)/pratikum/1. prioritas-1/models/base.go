912faa235db1ea1766f9cdb2e90c9497c6053c9f
Ganes556 <gustiganes@gmail.com>
2023-04-01 07:08:58 +0800

    [Tugas] fix 1.prioritas-1 model structure

---
+++ 21_ORM_and_Code_Structure_(MVC)/pratikum/1. prioritas-1/models/base.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+type Base struct {
+	ID        uint           `gorm:"primarykey" json:"id" form:"id"`
+	CreatedAt time.Time      `json:"-" form:"-"`
+	UpdatedAt time.Time      `json:"-" form:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-" form:"-"`
+}
--- 21_ORM_and_Code_Structure_(MVC)/pratikum/1. prioritas-1/models/user.go
+++ 21_ORM_and_Code_Structure_(MVC)/pratikum/1. prioritas-1/models/user.go
@@ -1,20 +1,8 @@
 package models
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
-
-type BaseModel struct {
-	ID        uint           `gorm:"primarykey" json:"id" form:"id"`
-	CreatedAt time.Time      `json:"-" form:"-"`
-	UpdatedAt time.Time      `json:"-" form:"-"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-" form:"-"`
-}
 
 type User struct {
-	BaseModel
+	Base
 	Name     string `json:"name" form:"name" validate:"required"`
 	Email    string `json:"email" form:"email" gorm:"index" validate:"required,email"`
 	Password string `json:"password" form:"password" validate:"required,min=8"`
