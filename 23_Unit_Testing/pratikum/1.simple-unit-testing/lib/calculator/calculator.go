b0aad18732450c3c42503cba2bb911259a573f4f
Ganes556 <gustiganes@gmail.com>
2023-04-05 16:08:55 +0800

    [Tugas] add unit test divide by zero

--- 23_Unit_Testing/pratikum/1.simple-unit-testing/lib/calculator/calculator.go
+++ 23_Unit_Testing/pratikum/1.simple-unit-testing/lib/calculator/calculator.go
@@ -1,17 +1,20 @@
 package lib
 
-func Addition(a int, b int) int {
-	return a + b
+func Addition(a int, b int) float64 {
+	return float64(a + b)
 }
 
-func Subtraction(a int, b int) int {
-	return a - b
+func Subtraction(a int, b int) float64 {
+	return float64(a - b)
 }
 
-func Multiplication(a int, b int) int {
-	return a * b
+func Multiplication(a int, b int) float64 {
+	return float64(a * b)
 }
 
-func Division(a int, b int) int {
-	return a / b
+func Division(a int, b int) float64 {
+	if b == 0 {
+		return float64(0)
+	}
+	return float64(a / b)
 }
--- 23_Unit_Testing/pratikum/1.simple-unit-testing/lib/calculator/calculator_test.go
+++ 23_Unit_Testing/pratikum/1.simple-unit-testing/lib/calculator/calculator_test.go
@@ -5,19 +5,19 @@ import "testing"
 func TestAddition(t *testing.T) {
 	result := Addition(1, 1)
 	if result != 2 {
-		t.Errorf("Addition(1, 1) = %d; want 2", result)
+		t.Errorf("Addition(1, 1) = %v; want 2", result)
 	}
 	result = Addition(-1, -1)
 	if result != -2 {
-		t.Errorf("Addition(-1, -1) = %d; want -2", result)
+		t.Errorf("Addition(-1, -1) = %v; want -2", result)
 	}
 	result = Addition(-1, 1)
 	if result != 0 {
-		t.Errorf("Addition(-1, 1) = %d; want 0", result)
+		t.Errorf("Addition(-1, 1) = %v; want 0", result)
 	}
 	result = Addition(1, -1)
 	if result != 0 {
-		t.Errorf("Addition(1, -1) = %d; want 0", result)
+		t.Errorf("Addition(1, -1) = %v; want 0", result)
 	}
 
 }
@@ -25,60 +25,64 @@ func TestAddition(t *testing.T) {
 func TestSubtraction(t *testing.T) {
 	result := Subtraction(1, 1)
 	if result != 0 {
-		t.Errorf("Subtraction(1, 1) = %d; want 0", result)
+		t.Errorf("Subtraction(1, 1) = %v; want 0", result)
 	}
 	result = Subtraction(-1, -1)
 	if result != 0 {
-		t.Errorf("Subtraction(-1, -1) = %d; want -2", result)
+		t.Errorf("Subtraction(-1, -1) = %v; want -2", result)
 	}
 	result = Subtraction(-1, 1)
 	if result != -2 {
-		t.Errorf("Subtraction(-1, 1) = %d; want -2", result)
+		t.Errorf("Subtraction(-1, 1) = %v; want -2", result)
 	}
 	result = Subtraction(1, -1)
 	if result != 2 {
-		t.Errorf("Subtraction(1, -1) = %d; want 2", result)
+		t.Errorf("Subtraction(1, -1) = %v; want 2", result)
 	}
 }
 
 func TestMultiplication(t *testing.T) {
 	result := Multiplication(2, 2)
 	if result != 4 {
-		t.Errorf("Multiplication(2, 2) = %d; want 4", result)
+		t.Errorf("Multiplication(2, 2) = %v; want 4", result)
 	}
 	result = Multiplication(-2, -2)
 	if result != 4 {
-		t.Errorf("Multiplication(-2, -2) = %d; want 4", result)
+		t.Errorf("Multiplication(-2, -2) = %v; want 4", result)
 	}
 	result = Multiplication(-2, 2)
 	if result != -4 {
-		t.Errorf("Multiplication(-2, 2) = %d; want -4", result)
+		t.Errorf("Multiplication(-2, 2) = %v; want -4", result)
 	}
 	result = Multiplication(0, 2)
 	if result != 0 {
-		t.Errorf("Multiplication(0, 2) = %d; want 0", result)
+		t.Errorf("Multiplication(0, 2) = %v; want 0", result)
 	}
 }
 
 func TestDivision(t *testing.T) {
 	result := Division(4, 2)
 	if result != 2 {
-		t.Errorf("Division(4, 2) = %d; want 2", result)
+		t.Errorf("Division(4, 2) = %v; want 2", result)
 	}
 	result = Division(-4, -2)
 	if result != 2 {
-		t.Errorf("Division(-4, -2) = %d; want 2", result)
+		t.Errorf("Division(-4, -2) = %v; want 2", result)
 	}
 	result = Division(-4, 2)
 	if result != -2 {
-		t.Errorf("Division(-4, 2) = %d; want -2", result)
+		t.Errorf("Division(-4, 2) = %v; want -2", result)
 	}
 	result = Division(4, -2)
 	if result != -2 {
-		t.Errorf("Division(4, -2) = %d; want -2", result)
+		t.Errorf("Division(4, -2) = %v; want -2", result)
 	}
 	result = Division(0, 2)
 	if result != 0 {
-		t.Errorf("Division(0, 2) = %d; want 0", result)
+		t.Errorf("Division(0, 2) = %v; want 0", result)
+	}
+	result = Division(2,0)
+	if result != 0 {
+		t.Error("Division(2,0) must be error cause division by zero")
 	}
 }
