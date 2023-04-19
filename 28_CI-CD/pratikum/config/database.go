029f9c14c803fecb5f16b895279e27a65f521c70
Ganes556 <gustiganes@gmail.com>
2023-04-19 21:26:20 +0800

    change db & add dockerfile

--- 28_CI-CD/pratikum/config/database.go
+++ 28_CI-CD/pratikum/config/database.go
@@ -2,13 +2,21 @@ package config
 
 import (
 	"belajar-go-echo/model"
+	"fmt"
+	"os"
 
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	return gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_NAME"),
+	)
+	return gorm.Open(mysql.Open(connectionString), &gorm.Config{})
 }
 
 func MigrateDB(db *gorm.DB) error {
